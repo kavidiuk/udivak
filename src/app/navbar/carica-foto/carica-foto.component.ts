028c2a5ae637cb5561fb8b80e5dc68db52bdbc63
kavidiuk <kaviduwarnakulasuriya@gmail.com>
2024-03-04 19:45:20 +0100

    Aggiunta di Componet

---
+++ src/app/navbar/carica-foto/carica-foto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carica-foto',
+  standalone: true,
+  imports: [],
+  templateUrl: './carica-foto.component.html',
+  styleUrl: './carica-foto.component.scss'
+})
+export class CaricaFotoComponent {
+
+}
---
+++ src/app/navbar/homeUtente/messaggi-diretti/messaggi-diretti.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-messaggi-diretti',
+  standalone: true,
+  imports: [],
+  templateUrl: './messaggi-diretti.component.html',
+  styleUrl: './messaggi-diretti.component.scss'
+})
+export class MessaggiDirettiComponent {
+
+}
