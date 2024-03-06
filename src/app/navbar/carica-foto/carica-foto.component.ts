3c74f2ba558bbbcebf8c0ec044519599007a07e6
kavidiuk <kaviduwarnakulasuriya@gmail.com>
2024-03-06 23:25:54 +0100

    Aggiunto prima interfaccia base dell'utente e Cerca

--- src/app/navbar/carica-foto/carica-foto.component.ts
+++ src/app/navbar/carica-foto/carica-foto.component.ts
@@ -2,11 +2,44 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-carica-foto',
-  standalone: true,
-  imports: [],
   templateUrl: './carica-foto.component.html',
-  styleUrl: './carica-foto.component.scss'
+  styleUrls: ['./carica-foto.component.scss']
 })
 export class CaricaFotoComponent {
+
+  constructor() {}
+
+  addPost(event: Event): void {
+    event.preventDefault();
+
+    // Get values from form
+    const title = (<HTMLInputElement>document.getElementById('title'))?.value;
+    const content = (<HTMLInputElement>document.getElementById('content'))?.value;
+
+    // Create a new post element
+    const postElement = document.createElement('div');
+    postElement.className = 'card mt-3';
+    postElement.innerHTML = `
+        <div class="card-body">
+            <h5 class="card-title">${title}</h5>
+            <p class="card-text">${content}</p>
+        </div>
+    `;
+
+    // Append the new post to the container
+    const postContainer = document.getElementById('postContainer');
+    if (postContainer) {
+      postContainer.appendChild(postElement);
+    }
+
+    // Clear the form
+    const titleInput = <HTMLInputElement>document.getElementById('title');
+    const contentInput = <HTMLInputElement>document.getElementById('content');
+
+    if (titleInput && contentInput) {
+      titleInput.value = '';
+      contentInput.value = '';
+    }
+  }
 
 }
