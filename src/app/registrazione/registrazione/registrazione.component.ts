7fb7dd9ddada9464f16814b816c66ab8e0e1341b
kavidiuk <kaviduwarnakulasuriya@gmail.com>
2024-03-21 17:52:01 +0100

    aggiunta di apiTest api test 1.0

--- src/app/registrazione/registrazione/registrazione.component.ts
+++ src/app/registrazione/registrazione/registrazione.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { response } from 'express';//questo
+import { ApiTest } from '../../apiTest';// *
 @Component({
   selector: 'app-registrazione',
   standalone: true,
@@ -8,6 +9,34 @@ import { RouterModule } from '@angular/router';
   templateUrl: './registrazione.component.html',
   styleUrl: './registrazione.component.scss'
 })
+// *
 export class RegistrazioneComponent {
 
+  nome: String | undefined;
+  cognome: String | undefined;
+  email: String | undefined;
+  passworld: String | undefined;
+  
+  constructor(private apiTest: ApiTest){}
+  
+  salvaDati(){
+    const datiUtenti = {
+      nome: this.nome,
+      cognome: this.cognome,
+      email: this.email,
+      passworld: this.passworld
+    };
+    this.apiTest.salvaDati(datiUtenti).subscribe(
+      (response: any) => {
+        console.log('Dati salvati con successo: ', response);
+      this.nome = '';
+      this.cognome = '';
+      this.email = '';
+      this.passworld = '';
+      },
+      (error: any) => {
+        console.error('Si è verificato un errore durante il salvataggio dei dati: ', error);
+      }
+    )
+  }
 }
