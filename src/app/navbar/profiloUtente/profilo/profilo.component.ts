cceb14f07435093d34721bb523d1a32262975b15
kavidiuk <kaviduwarnakulasuriya@gmail.com>
2024-03-25 22:26:33 +0100

    follower number

--- src/app/navbar/profiloUtente/profilo/profilo.component.ts
+++ src/app/navbar/profiloUtente/profilo/profilo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-profilo',
@@ -7,6 +7,16 @@ import { RouterModule } from '@angular/router';
   templateUrl: './profilo.component.html',
   styleUrl: './profilo.component.scss'
 })
-export class ProfiloComponent {
+export class ProfiloComponent implements OnInit{
 
+  constructor(){}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  generateRandomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  // post
+   
 }
+
